8125867568ccebeb5e6fd7e7142c83d0ec872c2f
shreyansh <shreyanshthakur2007@gmail.com>
2026-05-26 23:10:09 +0530

    basic ui implementation home interview page

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from 'react-router'
 import Login from './features/auth/pages/login'
 import Register from './features/auth/pages/register'
-import Home from './home'
+import Home from '../src/features/interview/Home'
 import Protected from './features/auth/components/Protected'
 
 
